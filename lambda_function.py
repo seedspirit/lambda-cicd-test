f24e21abf5bc291fa14aa342198b032ac94f8ca1
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-03-27 14:19:55 +0900

    lambda_handler.py

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ import os
 def find_code():
     INFO = {}
 
-    for naver_code in range(100, 20000):
+    for naver_code in range(100, 400):
         base_query = "https://pts.map.naver.com/end-subway/ends/web/{naver_code}/home".format(naver_code=naver_code)
         page = requests.get(base_query)
         soup = BeautifulSoup(page.text, "html.parser")
@@ -33,11 +33,12 @@ def find_code():
 def upload_file_to_s3(bucket_name, key, file_path):
     s3 = boto3.resource('s3')
     s3.meta.client.upload_file(file_path, bucket_name, key)
+    return True
 
 
 def download_data_from_s3(bucket_name, key, download_path):
     s3 = boto3.resource('s3')
-    download_path = 'data_from_s3.csv' # 다운로드 후에 어디에다가 위치시킬 것인지 경로
+    # 다운로드 후에 어디에다가 위치시킬 것인지 경로
     bucket = s3.Bucket(bucket_name)
     objects = list(bucket.objects.filter(Prefix=key))
     if objects and objects[0].key == key:
@@ -49,36 +50,26 @@ def download_data_from_s3(bucket_name, key, download_path):
 def delete_file_in_s3(bucket_name, key):
     s3 = boto3.resource('s3')
     s3.meta.client.delete_object(Bucket=bucket_name, Key=key)
+    return True
 
-
-def duplicate_check(bucket_name):
-    s3 = boto3.client('s3')
-    obj_list = s3.list_objects(Bucket=bucket_name)
-    key_list = obj_list['Contents']['Key']
-
-    if "bmt/subway_information.json" in key_list:
-        return True
-    else:
-        False
 
 
 
 # handler
-def handler(event, context):
+def lambda_handler(event, context):
     # 크롤링 후 json 파일 생성
-    with open('subway_information.json', 'w', encoding='utf-8') as f:
+    file = '/tmp/subway_information.json'
+    with open(file, 'w', encoding='utf-8') as f:
             json.dump(find_code(), f, ensure_ascii=False, indent=4)
 
     # 버킷에 저장 준비
-    bucket_name = "bucketestmyt"
+    bucket_name = "bucketestmy"
     key = "bmt/subway_information.json"
     file_path = "/tmp/subway_information.json" # lambda에서는 파일을 /tmp/에 저장
 
     s3 = boto3.client('s3')
 
-    if duplicate_check(bucket_name):
-        delete_file_in_s3(bucket_name, key)
-        upload_file_to_s3
+    result = upload_file_to_s3(bucket_name, key, file_path)
 
     if result:
         return {
@@ -90,9 +81,3 @@ def handler(event, context):
             'statusCode': 400,
             'body': json.dumps("upload fail")
         }
-
-
-# 최종 실행
-if __name__ == '__main__':
-    
-    handler()
