b277a1e04d897f369454ffe7ee42819433e73c0e
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-03-31 20:27:08 +0900

    Modify: lambda handler using multiprocessing

--- lambda_function.py
+++ lambda_function.py
@@ -1,30 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import time
+from time import sleep
+from multiprocessing import Pool, cpu_count
 import boto3
-import os
 
-# 네이버 시간표 복사하는 find_code 함수
+
+def get_station_info(naver_code):
+    base_query = "https://pts.map.naver.com/end-subway/ends/web/{naver_code}/home".format(naver_code=naver_code)
+    page = requests.get(base_query)
+    soup = BeautifulSoup(page.text, "html.parser")
+    try:
+        line_num = soup.select_one(
+            'body > div.app > div > div > div > div.place_info_box > div > div.p19g2ytg > div > button > strong.line_no').get_text()
+        station_nm = soup.select_one(
+            'body > div.app > div > div > div > div.place_info_box > div > div.p19g2ytg > div > button > strong.place_name').get_text()
+    except:
+        return None
+
+    return {"station_nm": station_nm, "naver_code": naver_code, "line_num": line_num}
+
+
 def find_code():
-    INFO = {}
+    with Pool(6) as p:
+        codes = range(100, 20000)
+        results = p.map(get_station_info, codes)
 
-    for naver_code in range(100, 20000):
-        base_query = "https://pts.map.naver.com/end-subway/ends/web/{naver_code}/home".format(naver_code=naver_code)
-        page = requests.get(base_query)
-        soup = BeautifulSoup(page.text, "html.parser")
-        try:
-            line_num = soup.select_one('body > div.app > div > div > div > div.place_info_box > div > div.p19g2ytg > div > button > strong.line_no').get_text()
-            station_nm = soup.select_one('body > div.app > div > div > div > div.place_info_box > div > div.p19g2ytg > div > button > strong.place_name').get_text()
-        except:
+    INFO = {}
+    for result in results:
+        if result is None:
             continue
 
-    # 호선 자체가 새로 생기는 경우가 있을 것 같아 호선을 미리 정의하지 않고, 크롤링 결과에 의해 정의되게 함
+        line_num = result["line_num"]
+        station_nm = result["station_nm"]
+        naver_code = result["naver_code"]
+
         if line_num not in INFO:
             INFO[line_num] = []
 
         block = {"station_nm": station_nm, "naver_code": naver_code}
         INFO[line_num].append(block)
-
 
     return INFO
 
